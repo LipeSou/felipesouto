51b6285baa451a8a8d751b08731427b891466516
LipeSou <felipesoutomdt@gmail.com>
2025-05-03 14:21:34 -0300

    fix: responsive

--- src/layout/DefaultLayout.tsx
+++ src/layout/DefaultLayout.tsx
@@ -6,14 +6,16 @@ export default function DefaultLayout() {
     <div className="min-h-screen bg-theme text-theme flex flex-col">
       <header className="p-8 flex items-center justify-between">
         <nav>
-          <Link to="/" className="font-theme text-2xl! font-bold!">
+          <Link to="/" className="font-theme text-xl md:text-2xl! font-bold!">
             Felipe Souto
           </Link>
         </nav>
-        <div className="cursor-pointer flex items-center ">
-          <span className="text-base mr-2">Navegue para...</span>
-          <div className="flex items-center justify-items-center border-[1px] border-solid fs-border px-1">
-            <Command height={16} />K
+        <div className="hidden md:block">
+          <div className="cursor-pointer flex items-center ">
+            <span className="text-base mr-2">Navegue para...</span>
+            <div className="flex items-center justify-items-center border-[1px] border-solid fs-border px-1">
+              <Command height={16} />K
+            </div>
           </div>
         </div>
       </header>
